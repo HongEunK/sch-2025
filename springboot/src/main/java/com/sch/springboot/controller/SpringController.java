b4727b2523e48d15efafdf344029f9f1411abe84
HongEunK <kiohg1030hg@gmail.com>
2025-08-26 10:38:43 +0900

    2025-08-26 commit

---
+++ springboot/src/main/java/com/sch/springboot/controller/SpringController.java
@@ -0,0 +1,18 @@
+package com.sch.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class SpringController {
+
+    @GetMapping("/spring")
+    public String spring(@RequestParam("name") String name, @RequestParam("age") int age, Model model) {
+        // 모델 객체에 추가 --> view 전달
+        model.addAttribute("name", name);
+        model.addAttribute("age", age);
+        return "spring"; // View name
+    }
+}
